2b4d488fb1ea5ccc3bae2ed29c1f399f5049dff9
Mohamed Emad <aleslam12345@gmail.com>
2024-06-20 14:48:43 +0300

    add about app section

---
+++ Hike_App/components/CustomListRow.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListRow.swift
+//  Hike_App
+//
+//  Created by Mohamed Emad on 20/06/2024.
+//
+
+import SwiftUI
+
+struct CustomListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListRow()
+}
